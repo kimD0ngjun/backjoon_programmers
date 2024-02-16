748626898061fd970a2c969ba9f05b49acc82865
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2024-02-16 16:25:11 +0900

    Time: 92 ms (56.52%), Space: 18.5 MB (61.96%) - LeetHub

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.py
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.py
@@ -1,38 +1,42 @@
-
+# 자력 x
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = collections.defaultdict(list)
         
         # 그래프 구성
-        for x, y in prerequisites:
-            graph[x].append(y)
+        for key, value in prerequisites:
+            graph[key].append(value)
 
-        traced = set()
-        visited = set()
+        trace = set() # 현재 분기에서의 추적 기록
+        visit = set() # 전체 분기에서의 방문 기록 남기기
 
-        def dfs(i):
+        def dfs(node):
             # 순환 구조이면 False
-            if i in traced:
+            if node in trace:
                 return False
 
-            # 이미 방문했던 노드이면
-            if i in visited:
+            # 이미 이전 분기에 방문해서 탐색의 필요성이 없는
+						# 즉, 사이클이 아니라는 확신을 얻은 노드이면
+            if node in visit:
                 return True
     
-            traced.add(i)
+            trace.add(node)
             
-            for y in graph[i]:
+            for y in graph[node]:
                 if not dfs(y):
                     return False
             
-            traced.remove(i)
+            trace.remove(node)
             # 탐색 종료 후 방문 노드 추가
-            visited.add(i)
+            visit.add(node)
+            
             return True
     
-        # 순환 구조 판별
-        for x in list(graph):
-            if not dfs(x):
+        # 각 시작점으로부터 순환 구조 여부 서치
+        for i in list(graph):
+            if not dfs(i):
                 return False
 
         return True
+    
+    # 반복문 풀이는 불가능한 것인가???
