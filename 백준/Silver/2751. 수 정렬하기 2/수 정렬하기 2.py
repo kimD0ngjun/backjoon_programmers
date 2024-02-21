635105384168a5e7f26eccad5b9d3b49ef82cf97
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2024-02-22 00:42:47 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1560 ms, Memory: 77192 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -16,4 +16,84 @@ result.sort()
 
 for i in range(count):
     print(result[i])
-
+    
+# import sys
+# 
+# # 입력 함수
+# def sys_input():
+#     return sys.stdin.readline().strip()
+# 
+# # 커스터마이징 병합 정렬 클래스
+# class merge_sort_custom:
+#     def __init__(self, array):
+#         self.array = array
+#         self.temp = [0] * len(array)  # 임시 배열을 인스턴스 변수로 초기화
+# 
+#     def merge_sort(self):
+#         self.sort(0, len(self.array) - 1)
+#         return self.array
+# 
+#     def sort(self, low_index, high_index):
+#         # 탈출 조건
+#         if low_index == high_index:
+#             return
+# 
+#         # 가운데 인덱스 추출
+#         middle_index = (high_index + low_index) // 2
+# 
+#         # 재귀 호출
+#         self.sort(low_index, middle_index)
+#         self.sort(middle_index + 1, high_index)
+# 
+#         # 병합(재귀 회수) 단계에서 정리될 부분
+#         self.merge(low_index, middle_index, high_index)
+# 
+#     # 별도의 배열화
+#     def merge(self, low_index, middle_index, high_index):
+#         temp_index = 0  # 임시 배열 인덱스
+#         left_pointer = low_index  # 쪼갠 배열 중 한쪽 포인터
+#         right_pointer = middle_index + 1  # 쪼갠 배열 중 다른쪽 포인터
+# 
+#         # 각 포인터가 담당 배열의 마지막 인덱스까지 닿을 때까지 수행
+#         while left_pointer <= middle_index and right_pointer <= high_index:
+#             if self.array[left_pointer] <= self.array[right_pointer]:
+#                 # 배열에 할당시킴과 동시에 변수 증가 업데이트(외워두기)
+#                 self.temp[temp_index] = self.array[left_pointer]
+#                 temp_index += 1
+#                 left_pointer += 1
+#             else:
+#                 self.temp[temp_index] = self.array[right_pointer]
+#                 temp_index += 1
+#                 right_pointer += 1
+# 
+#         # 한쪽 포인터가 먼저 다다랐을 때 수행
+#         while left_pointer <= middle_index:
+#             # 잔여 배열들 전부 붙여넣기
+#             self.temp[temp_index] = self.array[left_pointer]
+#             temp_index += 1
+#             left_pointer += 1
+# 
+#         # 얘도 마찬가지
+#         while right_pointer <= high_index:
+#             self.temp[temp_index] = self.array[right_pointer]
+#             temp_index += 1
+#             right_pointer += 1
+# 
+#         # 임시 배열의 내용을 원래 배열에 붙여넣기
+#         for i in range(temp_index):
+#             self.array[low_index + i] = self.temp[i]
+# 
+# 
+# # 입력 처리
+# count = int(sys_input())
+# 
+# input = []
+# 
+# for i in range(count):
+#     input.append(int(sys_input()))
+# 
+# custom_sort = merge_sort_custom(input)
+# result = custom_sort.merge_sort()
+# 
+# for i in range(count):
+#     print(result[i])
