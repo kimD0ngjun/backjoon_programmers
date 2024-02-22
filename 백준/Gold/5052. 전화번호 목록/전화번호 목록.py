47a9878b9b74fbc453b7b5ac6be4f303355b2719
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2024-02-22 14:26:10 +0900

    [Gold IV] Title: 전화번호 목록, Time: 176 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,35 @@
+import sys
+
+# 입력 함수
+def sys_input():
+    return sys.stdin.readline().strip()
+
+# 입력 처리
+tests = int(sys_input())
+result = []
+
+# 테스트케이스 단위
+for _ in range(tests):
+    test = int(sys_input())
+    cases = []
+
+    for i in range(test):
+        cases.append(sys_input())
+
+    # 각 테스트 케이스를 정렬
+    cases.sort()
+
+    prefix = False
+    for i in range(test - 1):
+        # 인접한 두 케이스끼리만 비교
+        if cases[i] == cases[i + 1][:len(cases[i])]:
+            prefix = True
+            break
+
+    if prefix:
+        result.append("NO")
+    else:
+        result.append("YES")
+
+for item in result:
+    print(item)
