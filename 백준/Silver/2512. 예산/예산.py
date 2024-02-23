fe960f24225d53fc0070fa32a12c8bb8cbda11c5
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2024-02-24 00:27:38 +0900

    [Silver II] Title: 예산, Time: 72 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -11,12 +11,12 @@ all_budgets = int(sys_input())
 provinces.sort()
 result = 0
 
-# 이진 탐색 커스터마이징
-## 흠... 왜 재귀로는 안 될까
-minimum, maximum = 0, max(provinces)
+# 재귀 커스터마이징 재도전
+def binary_search(provinces, all_budgets, minimum, maximum):
+    if minimum > maximum:
+        return maximum
 
-while minimum <= maximum:
-    limit = (maximum + minimum) // 2 # 상한액 계산
+    limit = (maximum + minimum) // 2  # 상한액 계산
     total_sum = 0
 
     # 상한액 이하의 예산을 모두 합산
@@ -26,10 +26,31 @@ while minimum <= maximum:
     # 상한액을 넘지 않으면 최대 상한액을 업데이트
     # 더 큰 상한액을 찾기 위해 범위를 늘림
     if total_sum <= all_budgets:
-        result = limit
-        minimum = limit + 1
+        return binary_search(provinces, all_budgets, limit + 1, maximum)  # 다음 탐색에서 중복된 중간값을 제외하기 위해 더하기 1
     # 그렇지 않으면 상한액 초과니까 최대 금액을 줄여서 다시 상한액 계산
     else:
-        maximum = limit - 1
+        return binary_search(provinces, all_budgets, minimum, limit - 1)  # 다음 탐색에서 중복된 중간값을 제외하기 위해 빼기 1
 
-print(result)
+
+# 이진 탐색 커스터마이징
+## 흠... 왜 재귀로는 안 될까
+minimum, maximum = 0, max(provinces)
+
+# while minimum <= maximum:
+#     limit = (maximum + minimum) // 2 # 상한액 계산
+#     total_sum = 0
+#
+#     # 상한액 이하의 예산을 모두 합산
+#     for budget in provinces:
+#         total_sum += min(limit, budget)
+#
+#     # 상한액을 넘지 않으면 최대 상한액을 업데이트
+#     # 더 큰 상한액을 찾기 위해 범위를 늘림
+#     if total_sum <= all_budgets:
+#         result = limit
+#         minimum = limit + 1 # 다음 탐색에서 중복된 중간값을 제외하기 위한 더하기 1
+#     # 그렇지 않으면 상한액 초과니까 최대 금액을 줄여서 다시 상한액 계산
+#     else:
+#         maximum = limit - 1 # 다음 탐색에서 중복된 중간값을 제외하기 위한 빼기 1
+
+print(binary_search(provinces, all_budgets, 0, max(provinces)))
