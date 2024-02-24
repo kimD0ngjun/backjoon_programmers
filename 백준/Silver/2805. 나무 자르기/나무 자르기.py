bde0b68519a65ec6c8d1dbd6f836db3a855c5e49
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2024-02-24 09:04:46 +0900

    [Silver II] Title: 나무 자르기, Time: 2236 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,37 @@
+import sys
+
+# 입력 함수
+def sys_input():
+    return sys.stdin.readline().strip()
+
+# 재귀 커스터마이징 재도전
+def binary_search(trees, need, minimum, maximum):
+    if minimum > maximum:
+        return maximum
+
+    cut = (maximum + minimum) // 2  # 필요 나무 최대 한정 높이 계산
+    total_sum = 0
+
+    for tree in trees:
+        if tree >= cut: # 나무가 커트라인 아래면 계산 x
+            total_sum += tree - cut
+
+    if total_sum >= need:
+        return binary_search(trees, need, cut + 1, maximum)
+    else:
+        return binary_search(trees, need, minimum, cut - 1)
+
+
+count, need = map(int, sys_input().split())
+trees = list(map(int, sys_input().split()))
+
+trees.sort()
+maximum = max(trees)
+minimum = 0
+
+# 뭔가 가장 짧은 나무를 min, 가장 긴 나무를 max로 두고
+# 필요한 나무 길이가 이진 탐색의 대상이 될 것 같다
+
+
+
+print(binary_search(trees, need, minimum, maximum))
