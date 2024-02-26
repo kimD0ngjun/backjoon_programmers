130c38aebaa173c182179c182a8e7e0bc6c7bc7e
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2024-02-26 15:59:58 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 128 ms, Memory: 36204 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -0,0 +1,67 @@
+import sys
+import heapq
+from collections import defaultdict
+
+graph = defaultdict(dict)
+
+
+# 입력 함수
+def sys_input():
+    return sys.stdin.readline().strip()
+
+
+# 입력 함수
+def sys_input():
+    return sys.stdin.readline().strip()
+
+
+# 다익스트라 함수
+def dijkstra(maps, length):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    inf = float('inf')
+    # 지도의 각 정점별 최단 가중치 초기화
+    min_distances = [[inf] * length for _ in range(length)]
+
+    queue = []
+    # 큐, 가중치, x좌표, y좌표
+    heapq.heappush(queue, (maps[0][0], 0, 0))
+    min_distances[0][0] = 0
+
+    while queue:
+        cur_weight, x, y = heapq.heappop(queue)
+
+        # 도착 백트랙킹(?)
+        if x == length - 1 and y == length - 1:
+            return min_distances[x][y]
+
+        for i in range(4):
+            next_x = x + dx[i]
+            next_y = y + dy[i]
+
+            if 0 <= next_x < length and 0 <= next_y < length:
+                updated_weight = cur_weight + maps[next_x][next_y]
+
+                if updated_weight < min_distances[next_x][next_y]:
+                    min_distances[next_x][next_y] = updated_weight
+                    heapq.heappush(queue, (updated_weight, next_x, next_y))
+
+
+## 입력 처리
+count = 0
+results = []
+
+while True:
+    length = int(sys_input())
+
+    if length == 0:
+        break
+
+    maps = [list(map(int, input().split())) for _ in range(length)]
+
+    result = dijkstra(maps, length)
+    results.append(result)
+    count += 1
+
+for i in range(0, count):
+    print(f'Problem {i + 1}: {results[i]}')
