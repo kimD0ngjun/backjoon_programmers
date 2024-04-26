ce45d805ddde00e7b29e6a7b71329ad0637dd437
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2024-04-26 10:09:04 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -6,21 +6,23 @@ def max_score(stairs):
         # 끝자락에 안착한 경우는 끝자락 값 추가
         if n == 0:
             return stairs[0]
-        # 한 칸 나아간 케이스의 최대갑 비교
+        # 두 개의 계단이 남은 시점에서 나아갈 방법
+        # 연속으로 두 칸 밟는 게 무조건 이득이니 연속으로 밟고 리턴시키기
         elif n == 1:
-            return max(stairs[0], stairs[0] + stairs[1])
-        # 두 칸 나아간 케이스의 최대값 비교
+            return stairs[0] + stairs[1]
+        # 세 계의 계단이 남은 시점에서의 나아갈 방법
+        # 2, 3 이렇게 밟거나 1, 3 이렇게 밟거나
         elif n == 2:
             return max(stairs[0] + stairs[2], stairs[1] + stairs[2])
 
-        # 이미 해당 계단 스텝이 메모이제이션에 존재하면 굳이 재귀 접근 x
+        # 이미 해당 계단 스텝의 점수값이 존재하면 메모이제이션에 존재하면 굳이 재귀 접근 x
         if n in memo:
             return memo[n]
 
         # 현재 계단을 밟는 경우와 밟지 않는 경우 중 큰 값 선택
-        step_one = stairs[n] + max(recursion(n - 2), recursion(n - 3) + stairs[n - 1])
-        memo[n] = step_one
-        return step_one
+        step = stairs[n] + max(recursion(n - 2), recursion(n - 3) + stairs[n - 1])
+        memo[n] = step
+        return step
 
     return recursion(len(stairs) - 1)
 
