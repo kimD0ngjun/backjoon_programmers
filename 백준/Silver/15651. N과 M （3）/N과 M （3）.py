255f9b5058d3ca923cd1bec144f7024d983a42d6
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2024-11-03 22:34:24 +0900

    [Silver III] Title: N과 M (3), Time: 1272 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+
+
+def backtrack(N, M, sequence):
+
+    if len(sequence) == M:
+        print(" ".join(map(str, sequence)))
+        return
+
+    for i in range(1, N + 1):
+        sequence.append(i)
+        backtrack(N, M, sequence)
+        sequence.pop()
+
+
+backtrack(N, M, [])
