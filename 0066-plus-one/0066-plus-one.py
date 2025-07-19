6c24e780f0eebd79940000ab2a250d835605150a
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2025-07-20 02:21:33 +0900

    Delete 0066-plus-one directory

--- 0066-plus-one/0066-plus-one.py
+++
@@ -1,8 +0,0 @@
-from functools import reduce
-
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        reducer = lambda digits: reduce(lambda acc, d: acc * 10 + d, digits)
-        value = reducer(digits) + 1
-        
-        return list(map(int, str(value)))
