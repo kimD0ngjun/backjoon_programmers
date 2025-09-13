84b3deeeea82392fe40e24e300732865623645f1
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2025-09-13 22:46:02 +0900

    [Platinum IV] Title: 열혈강호, Time: 728 ms, Memory: 124924 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    line = list(map(int, input().split()))
+    graph[i] = line[1:]
+
+match = [0] * (M + 1)
+
+def dfs(node):
+    if visited[node]:
+        return False
+    visited[node] = True
+
+    for adj in graph[node]:
+        if match[adj] == 0 or dfs(match[adj]):
+            match[adj] = node
+            return True
+        
+    return False
+
+result = 0
+for i in range(1, N + 1):
+    visited = [False] * (N + 1)
+    if dfs(i):
+        result += 1
+
+print(result)
