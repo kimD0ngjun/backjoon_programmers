4a5ee00b098d635a954e00715494e47ab2e650bf
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2025-09-25 21:37:18 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,21 +1,20 @@
-count = int(input())  # 수열의 길이
-array = list(map(int, input().split()))  # 주어진 수열
-
-# memoization
-# 각 요소별 시작 길이 카운팅에서 이전 머리에 해당하는 수열이 있으면 이어붙이는 식
-memo = [1] * count
+N = int(input())
+A = list(map(int, input().split()))
 
 """
-반복문
+2차원 리스트?
+memo[a][b] = a부터 b까지의 가장 긴 증가하는 부분 수열 길이 갱신?
+-> 시간복잡도 n**2
+
+memon[i] = 인덱스 i까지의 최단 배열 길이?
 """
-for i in range(1, count):
+
+memo = [1 for i in range(N)]
+
+for i in range(1, N):
     for j in range(0, i):
-        # 만일 기준 요소보다 이전의 요소들 중에 작은 값이 있으면
-        if array[j] < array[i]:
-            # 특정 지점에서 역순으로 바라보며 현 시점의 길이와 
-            # 이전 시점의 길이를 카운팅한 것들 중 큰 값으로 메모 업데이트
+        if A[i] > A[j]:
+            # 인덱스 j까지의 최대 길이 + 1
             memo[i] = max(memo[i], memo[j] + 1)
 
-# 메모들 요소가 곧 해당 요소까지의 증가분 길이
-result = max(memo)
-print(result)
+print(max(memo))
